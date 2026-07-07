98fffe7f796caac1d5b21c5ce15d0aeb0dcf6f16
Naveena-lanka <lankanaveena678@gmail.com>
2026-07-07 05:03:25 +0000

    Sync LeetCode submission Runtime - 55 ms (5.25%), Memory - 19 MB (95.77%)

--- 1205-defanging-an-ip-address/solution.py
+++ 1205-defanging-an-ip-address/solution.py
@@ -1,11 +1,4 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        ans=""
-        for i in address:
-            ch=i
-            if ch==".":
-                ans+="[.]"
-            else:
-                ans+=ch 
-        return ans       
-        
+        list1=address.split(".")
+        return "[.]".join(list1)
